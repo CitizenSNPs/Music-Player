2683bb7e60e6bab897c3c508073b7451617fd056
Derelle Kirksey <derelle.p.kirksey@gmail.com>
2018-03-12 11:51:27 -0700

    Format styles

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import songfile1 from "./Relax.mp3";
 import songfile2 from "./Reckoner.mp3";
 import songfile3 from "./Imaginary Parties.mp3"
 
-const songinfo = [{trackname:"Relax", artist:"Rezz", image:"http://www.youredm.com/wp-content/uploads/2017/06/rezz-face-first-rukes-1024x683.jpg",src:songfile1},
-		{trackname:"Reckoner", artist:"Radiohead", image:"http://diymag.com/media/img/General/Hall-of-Fame/_1500x1000_crop_center-center_75/tumblr_m8yak1u0a41qj3af5o1_1280.jpg",src:songfile2},
-		{trackname:"Unknown", artist:"Unknown", image:"https://ih0.redbubble.net/image.392413794.3911/flat,800x800,075,f.jpg",src:songfile3}];
+const songinfo = [{trackname:"Relax", artist:"Rezz", image:"http://www.youredm.com/wp-content/uploads/2017/06/rezz-face-first-rukes-1024x683.jpg",src:songfile1, number:0},
+		{trackname:"Reckoner", artist:"Radiohead", image:"http://diymag.com/media/img/General/Hall-of-Fame/_1500x1000_crop_center-center_75/tumblr_m8yak1u0a41qj3af5o1_1280.jpg",src:songfile2, number:1},
+		{trackname:"Unknown", artist:"Unknown", image:"https://ih0.redbubble.net/image.392413794.3911/flat,800x800,075,f.jpg",src:songfile3, number:2}];
 var songIndex = 0;
 
 class Controls extends React.Component{
@@ -19,7 +19,7 @@ class Controls extends React.Component{
                   {song:songfile2, playing:false, image:""}];
 
     this.state = {trackname: songinfo[songIndex].trackname, artist: songinfo[songIndex].artist,
-		song:songinfo[songIndex].src, playing:false, image:songinfo[songIndex].image};
+		song:songinfo[songIndex].src, playing:false, image:songinfo[songIndex].image, number: songinfo[songIndex].number};
 
 
 
@@ -36,20 +36,27 @@ class Controls extends React.Component{
 
   render(){
     return(<div>
-	  <div background={this.state.img} className="albumArtwork">
-    <div className="imageborder"><img src={this.state.image} /></div>
-	  <h2 id="songTitle">{this.state.trackname}</h2>
+
+    <div className="imageborder"><img src={this.state.image} />
+		<div id="info">
+	  <h2 id="songTitle">{this.state.trackname} </h2>
 	  <h3 id="artist">{this.state.artist}</h3>
+
+		<audio type="audio.mpeg" src= {this.state.song} ref="audio"/>
+		<input id="range" type="range" min="0" max="100" step="1" />
+
+		<div id="buttons">
+		<button id="back"onClick={this.songBack}></button>
+		<button id="play"onClick={this.playSong}></button>
+		<button id="forward"onClick={this.changeSong}></button>
+		</div>
 	  </div>
-          <audio type="audio.mpeg" src= {this.state.song} ref="audio"/>
-          <button onClick={this.songBack}>Back</button>
-          <button onClick={this.playSong}>Play/Pause</button>
-          <button onClick={this.changeSong}>Forward</button>
+		</div>
 					<select>
 						<option selected disabled hidden>Song Selection</option>
-						<option value="1">{songinfo[0].trackname} - {songinfo[0].artist}</option>
-						<option value="2">{songinfo[1].trackname} - {songinfo[1].artist}</option>
-						<option value="3">{songinfo[2].trackname} - {songinfo[2].artist}</option>
+						<option value="1" onChange={this.songSelect}>{songinfo[0].trackname} - {songinfo[0].artist}</option>
+						<option value="2" onChange={this.songSelect}>{songinfo[1].trackname} - {songinfo[1].artist}</option>
+						<option value="3" onChange={this.songSelect}>{songinfo[2].trackname} - {songinfo[2].artist}</option>
 
 					</select>
           </div>);
@@ -69,6 +76,9 @@ class Controls extends React.Component{
     changeSong(){
 			this.audio=document.querySelector("audio");
       console.log(songIndex);
+			if (songIndex == songinfo.length-1){
+				return (true)
+			}else{
       songIndex +=1;
       this.setState({
         song: songinfo[songIndex].src, image: songinfo[songIndex].image, trackname: songinfo[songIndex].trackname, artist: songinfo[songIndex].artist}, function(){
@@ -78,11 +88,15 @@ class Controls extends React.Component{
 
 					console.log(this.state);
 				});
+			};
       };
 
 		songBack(){
 			this.audio=document.querySelector("audio");
       console.log(songIndex);
+			if (songIndex == 0){
+				return (true)
+			}else{
       songIndex -=1;
       this.setState({
         song: songinfo[songIndex].src, image: songinfo[songIndex].image, trackname: songinfo[songIndex].trackname, artist: songinfo[songIndex].artist}, function(){
@@ -90,14 +104,12 @@ class Controls extends React.Component{
 					this.refs.audio.load();
 					this.refs.audio.play();
 		});
+	};
 
 		};
 
 		/*songSelect(){
-			this.audio = document.querySelector("audio");
-			var key = this.props.value;
-			songIndex = Number(key);
-			song: songinfo[songIndex].src, image: songinfo[songIndex].image, trackname: songinfo[songIndex].trackname, artist: songinfo[songIndex].artist}, function(){
+			this.audio = document.querySelector(`audio[data-key=${this.props.value}]`);
 				this.refs.audio.pause();
 				this.refs.audio.load();
 				this.refs.audio.play();
