0fffb71d3f5d456870db55ce96eefc6f1ed2abd4
Derelle Kirksey <derelle.p.kirksey@gmail.com>
2018-06-24 23:49:04 -0700

    refactoring to support backend

--- src/index.js
+++ src/index.js
@@ -1,38 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import songfile1 from "./Relax.mp3";
-import songfile2 from "./Reckoner.mp3";
-import songfile3 from "./Imaginary Parties.mp3"
+import HttpService from '../services/service';
 
-const songinfo = [{trackname:"Relax", artist:"Rezz", image:"http://www.youredm.com/wp-content/uploads/2017/06/rezz-face-first-rukes-1024x683.jpg",src:songfile1, number:0},
-		{trackname:"Reckoner", artist:"Radiohead", image:"http://diymag.com/media/img/General/Hall-of-Fame/_1500x1000_crop_center-center_75/tumblr_m8yak1u0a41qj3af5o1_1280.jpg",src:songfile2, number:1},
-		{trackname:"Unknown", artist:"Unknown", image:"https://ih0.redbubble.net/image.392413794.3911/flat,800x800,075,f.jpg",src:songfile3, number:2}];
-var songIndex = 0;
+const http = new HttpService();
+
 
 class Controls extends React.Component{
   constructor(props){
     super(props);
-
-    this.songs = [songfile1, songfile2, songfile3];
-    this.props = [{song:songfile1, playing:false, image: ""},
-                  {song:songfile2, playing:false, image:""}];
-
-    this.state = {trackname: songinfo[songIndex].trackname, artist: songinfo[songIndex].artist,
-		song:songinfo[songIndex].src, playing:false, image:songinfo[songIndex].image, number: songinfo[songIndex].number};
+		this.loadData();
 
 
 
-    this.audio = document.querySelector("audio");
+    this.state = {}
 
-
-
+		this.loadData = this.loadData.bind(this);
     this.playSong = this.playSong.bind(this);
     this.changeSong = this.changeSong.bind(this);
 		this.songBack = this.songBack.bind(this);
-		//this.songSelect = this.songSelect.bind(this);
+	}
 
-  };
+
+	// get songs from db
+	loadData = () => {
+		http.getSongs().then(songs => {
+			console.log(songs);
+		}, err => {
+			console.log("error!");
+
+		}
+	)}
 
   render(){
     return(<div>
