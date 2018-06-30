199c4e8c077ad919fa72b396fc2737a277b88778
Derelle Kirksey <derelle.p.kirksey@gmail.com>
2018-06-29 23:47:06 -0700

    styles and song info loaded

--- src/Controls.js
+++ src/Controls.js
@@ -7,20 +7,29 @@ import FaPauseCircle from 'react-icons/lib/fa/pause-circle';
 import FaFastForward from 'react-icons/lib/fa/fast-forward';
 import FaFastBackward from 'react-icons/lib/fa/fast-backward';
 import AudioSpectrum from 'react-audio-spectrum';
-import { FormattedTime, ProgressBar, TimeMarker } from 'react-player-controls'
-// import Visualizer from 'react-audio-visualizer';
 
 
 const http = new HttpService();
-const songList = [require('./MP3/Reckoner.mp3'), require('./MP3/ImaginaryParties.mp3')];
-const titles = ["Reckoner", "Unknown","BonJovi","Kazoo"];
+const songList = [require("./MP3/01 - Will Smith - Gettin' Jiggy Wit It.mp3"), require('./MP3/02 - Britney Spears -...Baby One More Time.mp3'),
+require('./MP3/03 - Christina Aguilera - Genie In A Bottle.mp3'),require('./MP3/04 - Brandy & Monica - The Boy Is Mine.mp3'),require('./MP3/05 - TLC - Waterfalls.mp3'),
+require('./MP3/06 - Salt-N-Pepa feat. En Vouge - Whatta Man.mp3'),require('./MP3/07 - Boyz II Men - Motownphilly.mp3'),require('./MP3/08 - Bell Biv DeVoe - Poison.mp3'),
+require('./MP3/09 - Wreckx-N-Effect - Rumpshaker.mp3'),require('./MP3/18 - Backstreet Boys - I Want It That Way.mp3')];
+const titles = ["Gettin'Jiggy Wit It", "Unknown","BonJovi","Kazoo"];
 
 class Controls extends React.Component{
   constructor(props){
     super(props);
 		this.loadData();
-    this.state = {playing: false, titles: [{artist: "Radiohead", name: "Reckoner", pic:require("./pics/Radiohead.jpg")},
-                                        {artist:"Unknown", name:"Imaginary Parties", pic:require("./pics/Rezz.jpg")}], songInfo:titles}
+    this.state = {playing: false, titles: [{artist: "Will Smith", name: "Gettin' Jiggy Wit It", pic:require("./pics/WillSmith.jpg")},
+                                        {artist:"Britney Spears", name:"...Baby One More Time", pic:require("./pics/Britney.png")},
+                                        {artist:"Christina Aguilera", name:"Genie In A Bottle", pic:require("./pics/Christina.jpg")},
+                                        {artist:"Brandy & Monica", name:"The Boy Is Mine", pic:require("./pics/Brandy.jpg")},
+                                        {artist:"TLC", name:"Waterfalls", pic:require("./pics/TLC.jpg")},
+                                        {artist:"Salt N Pepa", name:"Whatta Man", pic:require("./pics/Saltnpepa.jpg")},
+                                        {artist:"Boyz II Men", name:"Motownphilly", pic:require("./pics/Rezz.jpg")},
+                                        {artist:"Bell Biv Devoe", name:"Poison", pic:require("./pics/Rezz.jpg")},
+                                        {artist:"Wreckx-N-Effect", name:"Rumpshaker", pic:require("./pics/Rezz.jpg")},
+                                        {artist:"Backstreet Boys", name:"I Want It That Way", pic:require("./pics/Rezz.jpg")}], songInfo:titles, songTime:0}
     this.state.songIndex=this.props.songIndex;
     this.titles = function(){
       var list = [];
@@ -60,9 +69,11 @@ class Controls extends React.Component{
     let audio = songList[this.state.songIndex];
     var audio2 = document.querySelector('audio');
     audio2.src = (audio);
-    if (this.state.playing == true){
+    if (this.state.playing === true){
+      console.log(audio2.currentTime);
       audio2.pause();
       this.setState({playing: false});
+      console.log(audio2.currentTime);
     }else{
         audio2.play();
         this.setState({playing:true});
@@ -117,34 +128,34 @@ class Controls extends React.Component{
       <div id="background" /*style={{backgroundImage: `url(${this.state.titles[this.state.songIndex].pic})`}}*/>
       <div id="app">
       <div id="controls">
-      <FaFastBackward size={40} color="white" class="functions"onClick={this.prevSong} />
-      <FaPlayCircle class="functions" size={40} color="white" onClick={this.playSong} />
-      <FaPauseCircle class="functions" size={40} color="white"/>
-      <FaFastForward class="functions" size={40} color="white" onClick={this.nextSong}/>
+      <FaFastBackward size={30} color="white" class="functions"onClick={this.prevSong} />
+      <FaPlayCircle id="play" class="functions" size={30} color="white" onClick={this.playSong} />
+      <FaPauseCircle id="pause" class="functions" size={30} color="white"/>
+      <FaFastForward class="functions" size={30} color="white" onClick={this.nextSong}/>
       </div>
-      <div id="images">
+      <div id="top">
       <img id="mainImage" src={this.state.titles[this.state.songIndex].pic} />
       <AudioSpectrum
         id="audio-canvas"
-        height={100}
-        width={228}
+        height={25}
+        width={235}
         audioId={'audio-element'}
         capColor={'transparent'}
         capHeight={2}
         meterWidth={10}
         meterCount={512}
         meterColor={[
-          {stop: 0, color: 'rgba(255,250,250,0.5)'},
-          {stop: 0.5, color: 'white'},
+          {stop: 0, color: '#1f1f1f'},
+          {stop: 0.5, color: 'purple'},
           {stop: 1, color: 'red'}
         ]}
         gap={4}
       />
-      </div>
 
       <div id="titles">
       <h2>{this.state.titles[this.state.songIndex].name}</h2>
       <h3>{this.state.titles[this.state.songIndex].artist}</h3>
+      </div>
       </div>
       <audio id="audio-element" autoPlay src="" />
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Controls from "./Controls.js";
 
-const songList = [require('./MP3/Reckoner.mp3'), require('./MP3/ImaginaryParties.mp3')];
-const titles = ["Reckoner", "Unknown","BonJovi","Kazoo"];
+const songList = [require("./MP3/01 - Will Smith - Gettin' Jiggy Wit It.mp3"), require('./MP3/02 - Britney Spears -...Baby One More Time.mp3'),
+require('./MP3/03 - Christina Aguilera - Genie In A Bottle.mp3'),require('./MP3/04 - Brandy & Monica - The Boy Is Mine.mp3'),require('./MP3/05 - TLC - Waterfalls.mp3'),
+require('./MP3/06 - Salt-N-Pepa feat. En Vouge - Whatta Man.mp3'),require('./MP3/07 - Boyz II Men - Motownphilly.mp3'),require('./MP3/08 - Bell Biv DeVoe - Poison.mp3'),
+require('./MP3/09 - Wreckx-N-Effect - Rumpshaker.mp3'),require('./MP3/18 - Backstreet Boys - I Want It That Way.mp3')];
+const titles = ["Will Smith", "Britney Spears", "Christina Aguilera", "Brandy & Monica", "TLC", "Salt N Peppa", "Boyz II Men",
+"Bell Biv Devoe", "Wreckx-N-Effect", "Backstreet Boys"];
 
 class Sidebar extends React.Component{
   constructor(props){
